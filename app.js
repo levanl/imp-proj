2954313ca8f57836ffb9bfd56bd8b201060af5e3
levanloladze <levan.loladzew@gmail.com>
2023-06-04 19:09:55 +0400

    landing page main section

---
+++ app.js
@@ -0,0 +1,38 @@
+const text = document.querySelector('.fancy');
+
+const strText = text.textContent;
+
+const splitText = strText.split("");
+
+text.textContent = ""
+
+for (i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+
+    if( char === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+
+function complete() {
+    clearInterval(timer);
+    timer == null;
+}
+
+const logo = document.querySelectorAll('#logo path');
+
+for(let i = 0; i< logo.length; i++) {
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
